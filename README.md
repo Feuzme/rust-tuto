fa7b42524a71526958da05080f9ae88ac882694d
Feuzme <61864231+Feuzme@users.noreply.github.com>
2026-07-01 11:24:54 +0200

    Feat/guessing game (#1)
    feat: guessing game complete

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# rust-tuto
-- [rust-tuto](#rust-tuto)
+# RUST-TUTO
+- [RUST-TUTO](#rust-tuto)
   - [Description](#description)
+  - [Useful links](#useful-links)
+
 ## Description  
+<img src="./resources/learning_rustacean.webp" alt="Learning rustacean" wdth="100" height="100">
+
 This repo contains rust tutorial projects
+
+## Useful links
+- [The RUST programming language](https://doc.rust-lang.org/book/title-page.html)
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,39 @@
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number! ");
+
+    let secret_number = rand::random_range(1..=100);
+
+    let mut guess_input = String::new();
+    loop {
+        println!("Please input your guess : ");
+        io::stdin()
+            .read_line(&mut guess_input)
+            .expect("Failed to read line !");
+
+        println!("You guessed : {guess_input}");
+
+        let guess: u32 = match guess_input.trim().parse() {
+            Ok(num) => {
+                guess_input.clear();
+                num
+            }
+            Err(err) => {
+                println!("{err} : {guess_input}");
+                guess_input.clear();
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small !"),
+            Ordering::Greater => println!("Too big !"),
+            Ordering::Equal => {
+                println!("You win !");
+                break;
+            }
+        }
+    }
 }
