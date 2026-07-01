9c908db0e2e312b88ec41b2fff8aa2287dd52529
Feuzme <61864231+Feuzme@users.noreply.github.com>
2026-07-01 15:11:31 +0200

    test signed commits

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,13 +2,13 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number! ");
+    println!("Guess the number !");
 
     let secret_number = rand::random_range(1..=100);
 
     let mut guess_input = String::new();
     loop {
-        println!("Please input your guess : ");
+        println!("Please input your guess :");
         io::stdin()
             .read_line(&mut guess_input)
             .expect("Failed to read line !");
@@ -31,7 +31,7 @@ fn main() {
             Ordering::Less => println!("Too small !"),
             Ordering::Greater => println!("Too big !"),
             Ordering::Equal => {
-                println!("You win !");
+                println!("You win ! Awesome");
                 break;
             }
         }
