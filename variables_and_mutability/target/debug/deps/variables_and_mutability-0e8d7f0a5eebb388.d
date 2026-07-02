661f11634f615cc86a6b4ee080f24dda7c7e07ab
feuzme <feuzme@proton.me>
2026-07-02 10:36:13 +0200

    target ignore

--- variables_and_mutability/target/debug/deps/variables_and_mutability-0e8d7f0a5eebb388.d
+++
@@ -1,5 +0,0 @@
-/home/feuzme/dev/projects/rust-tuto/variables_and_mutability/target/debug/deps/variables_and_mutability-0e8d7f0a5eebb388.d: src/main.rs
-
-/home/feuzme/dev/projects/rust-tuto/variables_and_mutability/target/debug/deps/variables_and_mutability-0e8d7f0a5eebb388: src/main.rs
-
-src/main.rs:
--- variables_and_mutability/target/debug/variables_and_mutability.d
+++
@@ -1,1 +0,0 @@
-/home/feuzme/dev/projects/rust-tuto/variables_and_mutability/target/debug/variables_and_mutability: /home/feuzme/dev/projects/rust-tuto/variables_and_mutability/src/main.rs
